43f6fc951768eeba93e445d29925526884584408
Dhanpreet Singh <khalsads@users.noreply.github.com>
2020-04-16 16:11:26 -0400

    Updated CSS for home and JS to get google signin working

--- script/ds_script.js
+++ script/ds_script.js
@@ -1,6 +1,8 @@
 /* Function by google to retrieve profile information for a signed-in user */
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+  document.getElementById("home_signin").className = "d-none";
+  document.getElementById("home_signedin").className = "d-block";
   console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log("Name: " + profile.getName());
   console.log("Image URL: " + profile.getImageUrl());
